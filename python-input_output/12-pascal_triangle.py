55037d75f281b3fd62b9d37868e44f815dea823e
nyumaeric <e.nyumajr@alustudent.com>
2023-07-26 05:43:02 -0700

    12-pascal_triangle.py

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,30 +1,7 @@
-#!/usr/bin/python3
-"""
-===============================
-module with the class Student
-===============================
-"""
+#!/usr/bon/python3
 
 
-class Student:
-    """class with methods to_json for retrieves dictionary"""
 
-    def __init__(self, first_name, last_name, age):
-        """method for initialized all atributes"""
+for 2 less than n
 
-        self.first_name = first_name
-        self.last_name = last_name
-        self.age = age
-
-    def to_json(self, attrs=None):
-        """method for retrieve a dictionary representation for a
-        student instance"""
-
-        if attrs is None:
-            return self.__dict__
-        dic = {}
-        for key, value in self.__dict__.items():
-            for i in attrs:
-                if key == i:
-                    dic[key] = value
-        return dic
+add 1 to the end of the list
