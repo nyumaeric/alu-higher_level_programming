e322130176dde118f74e8b605df3bd452b835e40
nyumaeric <e.nyumajr@alustudent.com>
2023-07-26 04:28:45 -0700

    8-rectangle.py

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,19 +1,40 @@
 #!/usr/bin/python3
-"""Defines a class Rectangle that inherits from BaseGeometry."""
+"""Module that contains the class rectangle, a subclass of BaseGeometry"""
+
+
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """Represent a rectangle using BaseGeometry."""
+    """Subclass of BaseGeometry that describes a rectangle"""
 
     def __init__(self, width, height):
-        """Intialize a new Rectangle.
+        """Initialize the rectangle, validates width and height
 
         Args:
-            width (int): The width of the new Rectangle.
-            height (int): The height of the new Rectangle.
+            width (int): must be greater than 0
+            height (int: must be greater than 0
+
         """
+
         self.integer_validator("width", width)
-        self.__width = width
         self.integer_validator("height", height)
+        self.__width = width
         self.__height = height
+
+
+if __name__ == "__main__":
+    r = Rectangle(3, 5)
+
+    print(r)
+    print(dir(r))
+
+    try:
+        print("Rectangle: {} - {}".format(r.width, r.height))
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
+
+    try:
+        r2 = Rectangle(4, True)
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
