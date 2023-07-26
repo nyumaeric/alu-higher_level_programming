b4c1220a6b73bb0722aae82a3b7c250233ce51f7
nyumaeric <e.nyumajr@alustudent.com>
2023-07-26 05:57:42 -0700

    12-pascal_triangle.py

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,13 +1,11 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 
-def generate_list(n):
-  """Generates a list of integers from 1 to n, inclusive."""
-  result_list = []
-  for i in range(1, n + 1):
+n = 10  # Replace this with the desired value of n
+result_list = []
+
+for i in range(2, n):
     result_list.append(i)
-  return result_list
 
+result_list.append(1)
 
-if __name__ == "__main__":
-  n = 10
-  print(generate_list(n))
+print(result_list)
