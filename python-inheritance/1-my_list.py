4212be97980875eece5e753c2b456113e8ba1bbe
nyumaeric <e.nyumajr@alustudent.com>
2023-07-26 02:44:03 -0700

    1-my_list.py

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,13 +1,24 @@
-#!/usr/bin/python3)
+#!/usr/bin/python3
+""" module contaiing the class MyList that is a subclass of list"""
+
+
 class MyList(list):
+    """Contains an extra method to print an accending sort version of the list.
+    Assumes the list is make up of ints. """
+
     def print_sorted(self):
-        sorted_list = sorted(self)
-        print(sorted_list)
+        """ prints a sorted version of the MyList """
+        new_list = self.copy()
+        new_list.sort()
+        print(new_list)
 
-# Example usage:
 if __name__ == "__main__":
-    # Create an instance of MyList
-    my_list = MyList([5, 2, 8, 1, 3])
-
-    # Call the print_sorted method
+    my_list = MyList()
+    my_list.append(1)
+    my_list.append(4)
+    my_list.append(2)
+    my_list.append(3)
+    my_list.append(5)
+    print(my_list)
     my_list.print_sorted()
+    print(my_list)  
