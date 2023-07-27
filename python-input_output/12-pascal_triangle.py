69c1cd9c9451db1b5f9cfe30d0d428a4f6a686fa
nyumaeric <e.nyumajr@alustudent.com>
2023-07-27 02:25:33 -0700

    12-pascal_triangle.py

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,11 +1,25 @@
 #!/usr/bin/python3
+"""defines function to return Pascal's triangle"""
 
-n = 10  # Replace this with the desired value of n
-result_list = []
 
-for i in range(2, n):
-    result_list.append(i)
-
-result_list.append(1)
-
-print(result_list)
+def pascal_triangle(n):
+    """returns list representing Pascal's triangle"""
+    pascal_triangle = []
+    previous = [1]
+    if n <= 0:
+        return pascal_triangle
+    for row_number in range(n):
+        rowlist = []
+        if row_number == 0:
+            rowlist = [1]
+        else:
+            for i in range(row_number + 1):
+                if i == 0:
+                    rowlist.append(0 + previous[i])
+                elif i == (row_number):
+                    rowlist.append(previous[i - 1] + 0)
+                else:
+                    rowlist.append(previous[i - 1] + previous[i])
+        previous = rowlist
+        pascal_triangle.append(rowlist)
+    return pascal_triangle
