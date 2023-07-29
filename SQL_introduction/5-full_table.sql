4938c29d6b571c09f1ef8005563046e072a1f7d2
nyumaeric <e.nyumajr@alustudent.com>
2023-07-29 14:54:56 -0700

    5-full_table.sql

---
+++ SQL_introduction/5-full_table.sql
@@ -0,0 +1,3 @@
+-- Script to print the CREATE TABLE command for the table "first_table" in the database given.
+-- Querry to print the CREATE TABLE command for the table "first_table" in the database given.
+SHOW CREATE TABLE first_table;
