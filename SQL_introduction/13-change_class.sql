e81232266eb23ad832345331873294e78105bcc2
nyumaeric <e.nyumajr@alustudent.com>
2023-07-29 15:21:22 -0700

    13-change_class.sql

---
+++ SQL_introduction/13-change_class.sql
@@ -0,0 +1,3 @@
+-- Script to remove all records with a score <= 5 in the table "second_table" of the database provided.
+-- Querry to remove all records with a score <= 5 in the table "second_table" of the database provided.
+DELETE FROM second_table WHERE score <= 5;
