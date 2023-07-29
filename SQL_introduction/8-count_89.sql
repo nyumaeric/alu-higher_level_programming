465e9496893346183d99d66e169272df00aeeef0
nyumaeric <e.nyumajr@alustudent.com>
2023-07-29 15:13:54 -0700

    8-count_89.sql

---
+++ SQL_introduction/8-count_89.sql
@@ -0,0 +1,3 @@
+-- Script to display the number of records with id = 89 in the table first_table of the database provided.
+-- Querry to display the number of records with id = 89 in the table first_table of the database provided.
+SELECT COUNT("id") FROM first_table WHERE id = 89;
