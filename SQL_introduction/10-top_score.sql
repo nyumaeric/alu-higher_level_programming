9e7fc5c925b0c71d54c56cca51b7006455ca89a6
nyumaeric <e.nyumajr@alustudent.com>
2023-07-29 15:16:58 -0700

    10-top_score.sql

---
+++ SQL_introduction/10-top_score.sql
@@ -0,0 +1,3 @@
+-- Script to list all records of the table "second_table" of the database provided.
+-- Querry to list all records of the table "second_table" of the database provided.
+SELECT score, name FROM second_table ORDER BY score DESC;
