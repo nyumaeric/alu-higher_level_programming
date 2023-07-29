96abd5457e5e8f6b17377b3e511e68de5897aefe
nyumaeric <e.nyumajr@alustudent.com>
2023-07-29 14:56:39 -0700

    6-list_values.sql

---
+++ SQL_introduction/6-list_values.sql
@@ -0,0 +1,4 @@
+-- Script to list all rows of the table first_table from the database given.
+-- Querry to  list all rows of the table first_table from the database given.
+SELECT * FROM first_table;
+
