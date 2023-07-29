65acd2985626b9ff1a34ac2a27456b99d99afce3
nyumaeric <e.nyumajr@alustudent.com>
2023-07-29 15:26:11 -0700

    15-groups.sql

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,3 +1,4 @@
 -- Script to list the number of records with the same score in the table "second_table" of the database provided.
 -- Querry to list the number of records with the same score in the table "second_table" of the database provided.
 SELECT score, COUNT(score) AS "number" FROM second_table GROUP BY score DESC;
+
