9330b2a2ab5152766fd92879b1083e81d5f934a3
nyumaeric <e.nyumajr@alustudent.com>
2023-08-01 03:37:50 -0700

    3-force_name.sql

---
+++ SQL_more_queries/3-force_name.sql
@@ -0,0 +1,6 @@
+-- Script that creates the table force_name in database provided.
+-- Query that creates the table force_name in database provided.
+CREATE TABLE IF NOT EXISTS force_name(
+    id INT,
+    name VARCHAR(256) NOT NULL
+);
