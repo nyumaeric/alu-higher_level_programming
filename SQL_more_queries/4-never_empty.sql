909c23c65e77855a8033482965dbc89fbd56c64b
nyumaeric <e.nyumajr@alustudent.com>
2023-08-01 03:39:00 -0700

    4-never_empty.sql

---
+++ SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,6 @@
+-- Script that creates the table id_not_null in database provided.
+-- Query that creates the table.
+CREATE TABLE IF NOT EXISTS id_not_null(
+    id INT DEFAULT 1,
+    name VARCHAR(256)
+);
