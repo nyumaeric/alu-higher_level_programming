e2c3c2ffec1556cce136d593f2a04f56050fd90e
nyumaeric <e.nyumajr@alustudent.com>
2023-08-03 12:40:03 -0700

    0-hbtn_status.py

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-""" a Python script that fetches https://intranet.hbtn.io/status """
-import urllib.request
-
-
-if __name__ == '__main__':
-    req = urllib.request.Request('https://intranet.hbtn.io/status')
-    with urllib.request.urlopen(req) as response:
+"""
+Python script that fetches https://intranet.hbtn.io/status
+"""
+if __name__ == "__main__":
+    import urllib.request as request
+    with request.urlopen('https://intranet.hbtn.io/status') as response:
+        content = response.read()
+        utf8_content = content.decode("UTF-8")
         print("Body response:")
-        html = response.read()
-        print("\t- type:", type(html))
-        print("\t- content:", html)
-        print("\t- utf8 content:", html.decode('utf-8'))
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(utf8_content))
