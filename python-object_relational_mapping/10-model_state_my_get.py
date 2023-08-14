5bc39bcfb0a98aa52cd2f2c6ca9782e943e69510
nyumaeric <e.nyumajr@alustudent.com>
2023-08-14 12:11:42 -0700

    10-model_state_my_get.py

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -1,25 +1,23 @@
 #!/usr/bin/python3
-""" Script that prints the State object with the name passed as an argument
-to the database hbtn_0e_6_usa. Takes 4 arguments: mysql username,
-mysql password, database name and name.
 """
-
-import sys
+lists State object from the database that matches argument given
+using SQLAlchemy and importing State and Base from model_state
+"""
+from sys import argv
 from model_state import Base, State
+
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost/{}'.format(
-            sys.argv[1], sys.argv[2], sys.argv[3]))
-    Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    State = session.query(State).\
-        filter(State.name == sys.argv[4]).first()
-    if State:
-        print("{}".format(state.id))
-    else:
-        print("Not found")
-    session.close
+        engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+            argv[1], argv[2], argv[3]), pool_pre_ping=True)
+        Base.metadata.create_all(engine)
+        Session = sessionmaker(bind=engine)
+        session = Session()
+        for instance in session.query(State).filter_by(name=argv[4]):
+                print("{}".format(instance.id))
+                break
+        else:
+                print("Not found")
+        session.close()
