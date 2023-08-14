1153407b629456a123e6d44204c057b30b8620fa
nyumaeric <e.nyumajr@alustudent.com>
2023-08-14 12:21:56 -0700

    10-model_state_my_get.py

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -1,23 +1,37 @@
 #!/usr/bin/python3
 """
-lists State object from the database that matches argument given
-using SQLAlchemy and importing State and Base from model_state
+    A script that prints the State object with the name passed as an argument
+    from hbtn_0e_6_usa
+    Username, password, dbname and name to search
+    will be passed as arguments to the script.
 """
-from sys import argv
+
+
+import sys
 from model_state import Base, State
-
-from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
 
-if __name__ == "__main__":
-        engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-            argv[1], argv[2], argv[3]), pool_pre_ping=True)
-        Base.metadata.create_all(engine)
-        Session = sessionmaker(bind=engine)
-        session = Session()
-        for instance in session.query(State).filter_by(name=argv[4]):
-                print("{}".format(instance.id))
-                break
-        else:
-                print("Not found")
-        session.close()
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+                           sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+
+    Base.metadata.create_all(engine)
+
+    # create a session
+    session = Session()
+
+    # extract first state
+    states = session.query(State) \
+                    .filter(State.name == sys.argv[4]).one_or_none()
+
+    # print state.id
+    if states is None:
+        print("Not found")
+    else:
+        print(states.id)
+
+    session.close()
