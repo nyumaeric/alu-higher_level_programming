7372aed6d3d20edd1d7302cb5fbd39a5d137f196
nyumaeric <e.nyumajr@alustudent.com>
2023-09-25 04:54:04 -0700

    0-javascript_is_amazing.js

--- javascript-warm_up/0-javascript_is_amazing.js
+++ javascript-warm_up/0-javascript_is_amazing.js
@@ -2,5 +2,5 @@
 // Create a constant variable called `myVar` with the value "JavaScript is amazing"
 const myVar = "JavaScript is amazing";
 
-// Use `console.log()` to print the value of `myVar`
+// Use `console.log('JavaScript is amazing')` to print the value of `myVar`
 console.log(myVar);
