0d6cab582225ffb983ad1c7a0a67b47fbc0b4dbc
nyumaeric <e.nyumajr@alustudent.com>
2023-09-25 05:26:05 -0700

    8-square.js

---
+++ javascript-warm_up/8-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const num = parseInt(process.argv[2]);
+if (num) {
+  for (let i = 0; i < num; i++) {
+    let str = '';
+    for (let j = 0; j < num; j++) {
+      str += 'X';
+    }
+    console.log(str);
+  }
+} else {
+  console.log('Missing size');
+}
